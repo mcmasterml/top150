2ce4a839a7ce0c30f049f607ad8462ed7e7248ac
Peter McMaster <39925732+mcmasterml@users.noreply.github.com>
2024-01-02 13:13:05 -0800

    Add 011 soln.

--- 011/solution.py
+++ 011/solution.py
@@ -12,15 +12,61 @@
 # Notice that you may not slant the container.
 
 
-# Exapmle 1: 
+# Exapmle 1:
 # Input: height = [1,8,6,2,5,4,8,3,7]
 # Output: 49
-# Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. 
+# Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7].
 # In this case, the max area of water (blue section) the container can contain is 49.
 
 # Example 2:
 # Input: height = [1,1]
 # Output: 1
 
+from typing import List
+
+
 class Solution:
-  def maxArea(self, height: List[int]) -> int:
+    def maxArea(self, height: List[int]) -> int:
+
+        # brute force soln.
+
+        # position1 = -1
+        # position2 = -1
+        # maxArea = 0
+
+        # for i in range(len(height)):
+        #     for j in range(i + 1, len(height)):
+        #         tall = min(height[i], height[j])
+        #         area = (j - i) * tall
+        #         if area > maxArea:
+        #             position1 = i
+        #             position2 = j
+        #             maxArea = area
+        # return maxArea
+
+        # using left/right pointers moving inwards
+        maxArea = 0
+        left = 0
+        right = len(height) - 1
+
+        while left < right:
+            area = min(height[left], height[right]) * (right - left)
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+            if area > maxArea:
+                maxArea = area
+
+        return maxArea
+
+
+s = Solution()
+
+
+def testCase01():
+    height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
+    assert s.maxArea(height) == 49
+
+
+testCase01()
--- 011/test.py
+++ 011/test.py
@@ -1,2 +1,10 @@
 import pytest
 from solution import Solution
+
+
+s = Solution()
+
+
+def testCase01():
+    height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
+    assert s.maxArea(height) == 49
