c4db96d94b0a112cb4321841ecef7fa9cba6e8ae
Peter McMaster <39925732+mcmasterml@users.noreply.github.com>
2024-01-03 12:34:09 -0800

    Begin 151 Reverse words in a string

---
+++ 151/solution.py
@@ -0,0 +1,26 @@
+# 151. Reverse Words in a String
+
+# Given an input string s, reverse the order of the words.
+
+# A word is defined as a sequence of non-space characters.
+# The words in s will be separated by at least one space.
+
+# Return a string of the words in reverse order concatenated by a single space.
+
+# Note that s may contain leading or trailing spaces or multiple spaces between two words.
+# The returned string should only have a single space separating the words. Do not include any extra spaces.
+
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        words = s.strip().split(" ")
+        words = words[::-1]
+        myString = ""
+        for word in words:
+            word.strip()
+            myString += word
+            if words[-1] != word:
+                myString += " "
+        return myString
+
+        # This IS INCORRECT CURRENTLY
---
+++ 151/test.py
@@ -0,0 +1,4 @@
+import pytest
+from solution import Solution
+
+s = Solution()
